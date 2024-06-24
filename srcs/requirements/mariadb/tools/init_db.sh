e2b7244d91072fdbcd7a75c96ae21e60ae69020a
fursini <francoursinikbw@gmail.com>
2024-06-25 00:53:52 +0200

    work in progress

---
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+mysql_install_db
+
+sleep 5
+
+/usr/bin/mysqld_safe &
+
+sleep 5
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then
+	echo "Database already exists"
+else
+	mysql_secure_installation << EOF
+
+	Y
+	Y
+	$MYSQL_ROOT_PASSWORD
+	$MYSQL_ROOT_PASSWORD
+	Y
+	n
+	Y
+	Y
+EOF
+
+	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+	mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+fi
+
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+
+exec "$@"
